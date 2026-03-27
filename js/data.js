3672a5387bcd9b825eadcc22ce2fa58b5458e1ca
Rafli Halomoan <raflizamoon@gmail.com>
2026-03-27 10:33:19 +0700

    first commit

---
+++ js/data.js
@@ -0,0 +1,30 @@
+const films = [
+  {
+    id: 1,
+    title: "Pacific Rim",
+    price: 35000,
+    days: ["Monday", "Tuesday", "Wednesday"],
+    times: ["14:00", "18:00", "21:00"]
+  },
+  {
+    id: 2,
+    title: "Avengers: Endgame",
+    price: 40000,
+    days: ["Thursday", "Friday", "Saturday"],
+    times: ["15:00", "19:00", "22:00"]
+  },
+  {
+    id: 3,
+    title: "Spider-Man: No Way Home",
+    price: 40000,
+    days: ["Monday", "Wednesday", "Sunday"],
+    times: ["13:00", "17:00", "20:00"]
+  },
+  {
+    id: 4,
+    title: "The Dark Knight",
+    price: 30000,
+    days: ["Tuesday", "Friday", "Saturday"],
+    times: ["16:00", "19:30", "22:30"]
+  }
+];
