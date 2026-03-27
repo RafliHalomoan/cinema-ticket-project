e8a2329d671cf5f0c10fdb80356b95c45340e47b
Rafli Halomoan <raflizamoon@gmail.com>
2026-03-27 14:23:32 +0700

    tambahan gambar

--- js/data.js
+++ js/data.js
@@ -2,13 +2,15 @@ const films = [
   {
     id: 1,
     title: "Pacific Rim",
+    image: "assets/PasificRim.jpeg",
     price: 35000,
     days: ["Monday", "Tuesday", "Wednesday"],
     times: ["14:00", "18:00", "21:00"]
   },
   {
     id: 2,
-    title: "Avengers: Endgame",
+    title: "Transformers",
+    image: "assets/Transformers.jpeg",
     price: 40000,
     days: ["Thursday", "Friday", "Saturday"],
     times: ["15:00", "19:00", "22:00"]
@@ -16,6 +18,7 @@ const films = [
   {
     id: 3,
     title: "Spider-Man: No Way Home",
+    image: "assets/Spider-Man.jpeg",
     price: 40000,
     days: ["Monday", "Wednesday", "Sunday"],
     times: ["13:00", "17:00", "20:00"]
@@ -23,6 +26,7 @@ const films = [
   {
     id: 4,
     title: "The Dark Knight",
+    image: "assets/TheDarkKnight.jpeg",
     price: 30000,
     days: ["Tuesday", "Friday", "Saturday"],
     times: ["16:00", "19:30", "22:30"]
